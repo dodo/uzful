21fc752cb2e4dd0fbf68cbc248cae4a62c1e6a93
dodo <dodo@blacksec.org>
2011-02-07 01:18:35 +0100

    add patch func for vicious to enable cache for all register

--- util.lua
+++ util.lua
@@ -10,3 +10,18 @@ function wibox(args)
     w.visible = args.visible or false
     return w
 end
+
+patch = {
+    vicious = function (vicious)
+        local cache = {}
+        local register = vicious.register
+        vicious.register = function (widget, wtype, format, interval, warg)
+            if cache[wtype] == nil then
+                cache[wtype] = 1
+                vicious.cache(wtype)
+            end
+            register(widget, wtype, format, interval, warg)
+        end
+    end,
+    }
+
