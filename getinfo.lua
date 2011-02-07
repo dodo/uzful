a932ab15663fd8940b1ecc281096a1469ee7b0a9
john stone <john@tuxcode.org>
2011-02-07 03:03:47 +0100

    mhh mit zähl und komische stats

--- getinfo.lua
+++ getinfo.lua
@@ -36,9 +36,44 @@ function get_interfaces()
 end
 
 
+function john_num_cpus()
+	local f = io.open("/proc/cpuinfo","r")
+	local out = assert(f:read("*a"))
+	f:close()
+	local i = 0 
+	for w in string.gfind(out, "processor	: %d+") do
+		i = i +1
+	end
+	return i
+end
 
 
+function local_cpus()
+
+	local cpu_lines = {}
+
+	-- Get CPU stats
+	for line in io.lines("/proc/stat") do
+		if string.find(line, "^cpu%d+") then
+			cpu_lines[#cpu_lines+1] = {}
+
+			for i in string.gmatch(line, "[%s]+([%d]+)") do
+				table.insert(cpu_lines[#cpu_lines], i)
+			end
+		end
+	end
+	return cpu_lines
+end
+
+
+
+print "---------------------"
 print (vardump(get_interfaces()))
+print "---------------------"
 
+print (vardump(john_num_cpus()))
+print "---------------------"
+
+print (vardump(local_cpus()))
 
 
