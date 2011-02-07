ff05aaac25af19bf9a5df4110826ac2b4ae7c954
dodo <dodo@blacksec.org>
2011-02-07 03:18:31 +0100

    uzful.widget.set_properties

--- widget.lua
+++ widget.lua
@@ -1,5 +1,7 @@
 
 local Wibox = require("wibox")
+local pairs = pairs
+local print = print
 
 module("uzful.widget")
 
@@ -8,5 +10,16 @@ function wibox(args)
     w.screen = args.screen or 1
     w.visible = args.visible or false
     return w
+end
+
+function set_properties(widget, properties)
+    local fun = nil
+    for name, property in pairs(properties) do
+        fun = widget["set_" .. name]
+        if fun then
+            fun(widget, property)
+        end
+    end
+    return widget
 end
 
