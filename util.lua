24e737221ad94e00b371423e75e2ab5a4f831e0b
dodo <dodo@blacksec.org>
2011-02-07 03:05:36 +0100

    moved wibox from uzful.util to uzful.widget

--- util.lua
+++ util.lua
@@ -1,16 +1,8 @@
 
 
 local awful = require("awful")
-local Wibox = require("wibox")
 
 module("uzful.util")
-
-function wibox(args)
-    local w = Wibox(args)
-    w.screen = args.screen or 1
-    w.visible = args.visible or false
-    return w
-end
 
 patch = {
     vicious = function (vicious)
