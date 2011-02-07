4f44945625e90546c8e2f4e8d147940a2f74d52b
john stone <john@tuxcode.org>
2011-02-07 02:37:38 +0100

    jeah

---
+++ getinfo.lua
@@ -0,0 +1,44 @@
+--
+--------------------------------------------------------------------------------
+--         FILE:  getinfo.lua
+--        USAGE:  ./getinfo.lua 
+--  DESCRIPTION:  
+--      OPTIONS:  ---
+-- REQUIREMENTS:  ---
+--         BUGS:  ---
+--        NOTES:  ---
+--       AUTHOR:   (), <>
+--      COMPANY:  
+--      VERSION:  1.0
+--      CREATED:  07.02.2011 02:09:01 CET
+--     REVISION:  ---
+--------------------------------------------------------------------------------
+--
+
+-- while debug: --
+require("vardump")
+
+
+
+function get_interfaces()
+	local f = io.popen("ls /sys/class/net", "r")
+	local out = assert(f:read("*a"))
+	f:close()
+	local ret = {}
+	for w in string.gfind(out, "%w+") do
+		if not ( string.find(w,"lo") or
+		 string.find(w, "dummy%d+") ) 
+		 then
+			 table.insert(ret, w)
+		 end
+	end
+	return ret
+end
+
+
+
+
+print (vardump(get_interfaces()))
+
+
+
---
+++ vardump.lua
@@ -0,0 +1,49 @@
+
+
+function vardump(data)
+	local function rvardump (rdata, erg,indent,key)
+
+		local linePrefix = ""
+		if key ~= nil then 
+			linePrefix = string.format("[%s]",key)
+		end
+
+		if(indent == nil) then
+			indent = 0
+		else 
+			indent = indent +1
+			table.insert(erg,string.rep(" ",indent)) 
+		end
+
+		if type(rdata) == 'table' then
+			mTable = getmetatable(rdata)
+			if mTable == nil then
+				table.insert(erg,linePrefix)
+				table.insert(erg ,"(table)")
+			else
+				table.insert(erg ,"(metatable)")
+				rdata = mTable
+			end		
+			table.insert(erg,"\n")
+			for tableKey, tableValue in pairs(rdata) do
+				rvardump(tableValue, erg,indent,tableKey)
+			end
+		elseif  type(rdata) == 'function'   or
+			type(rdata)	== 'thread' or 
+			type(rdata)	== 'userdata' or 
+			rdata		== nil  then
+
+			table.insert(erg,tostring(rdata))
+			table.insert(erg,"\n")
+		else
+			table.insert(erg,string.format("%s(%s)%s",linePrefix,type(rdata),tostring(rdata)))
+			table.insert(erg,"\n")
+		end
+	end
+
+	local erg= {}
+	rvardump(data,erg)
+	
+	return table.concat(erg)
+end
+
