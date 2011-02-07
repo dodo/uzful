f3cf3a478047c57b15bc343af61421bc0a4c16d7
dodo <dodo@blacksec.org>
2011-02-07 05:21:52 +0100

    uzful.widget.netgraphs now with optional big or small graphs

--- widget.lua
+++ widget.lua
@@ -37,12 +37,15 @@ end
 
 
 function netgraphs(args)
+    local ret = {}
     for _, size in ipairs({"small", "big"}) do
-        for _, typ in ipairs({"down", "up"}) do
-            for _, ground in ipairs({"fg", "bg"}) do
-                args[size][typ .. '_' .. ground .. "color"] =
-                    args[size][typ .. '_' .. ground .. "color"] or
-                          args[typ .. '_' .. ground .. "color"]
+        if args[size] then
+            for _, typ in ipairs({"down", "up"}) do
+                for _, ground in ipairs({"fg", "bg"}) do
+                    args[size][typ .. '_' .. ground .. "color"] =
+                        args[size][typ .. '_' .. ground .. "color"] or
+                            args[typ .. '_' .. ground .. "color"]
+                end
             end
         end
     end
@@ -52,81 +55,88 @@ function netgraphs(args)
     for k, v in ipairs(network_interfaces) do
         interface_cache[v] = k
     end
-
-    local small_layout = {}
-    local small_widgets = {}
-    local small_geometry = {width=args.small.width, height=args.small.height/2}
-    for _, interface in ipairs(network_interfaces) do
-        local l = Wibox.layout.fixed.vertical()
-        for _, typ in ipairs({"down", "up"}) do
-            local g = awful.widget.graph(small_geometry)
-            set_properties(g, {
-                border_color = nil,
-                color = args.small[typ .. '_fgcolor'],
-                background_color = args.small[typ .. '_bgcolor'] })
-            vicious.register(g, vicious.widgets.net,
-                "${" .. interface .. " " .. typ .. "_kb}", 2)
-            table.insert(small_widgets, g)
-            l:add(g)
-        end
-        small_layout[interface] = l
-    end
-
     local cur = network_interfaces[1]
-    local small = Wibox.layout.fixed.horizontal()
-    small:add(small_layout[cur])
 
-    local if_text = function (interface)
-        return interface == cur and (' <span size="x-small"><b>' ..
-            interface .. '</b></span>') or (
-            ' <span color="#666666" size="x-small">' ..
-            interface .. '</span>')
-    end
-
-    local labels = {}
-    small:connect_signal("button::release", function ()
-        cur = network_interfaces[interface_cache[cur] % #network_interfaces + 1]
-        small:reset()
-        small:add(small_layout[cur])
+    local small = nil
+    local small_layout = {}
+    if args.small then
+        local small_widgets = {}
+        local small_geometry={width=args.small.width,height=args.small.height/2}
         for _, interface in ipairs(network_interfaces) do
-            labels[interface]:set_markup(if_text(interface))
+            local l = Wibox.layout.fixed.vertical()
+            for _, typ in ipairs({"down", "up"}) do
+                local g = awful.widget.graph(small_geometry)
+                set_properties(g, {
+                    border_color = nil,
+                    color = args.small[typ .. '_fgcolor'],
+                    background_color = args.small[typ .. '_bgcolor'] })
+                vicious.register(g, vicious.widgets.net,
+                    "${" .. interface .. " " .. typ .. "_kb}", 2)
+                table.insert(small_widgets, g)
+                l:add(g)
+            end
+            small_layout[interface] = l
         end
-    end)
 
-    local height = 0
-    local big_widgets = {}
-    local big = Wibox.layout.fixed.vertical()
-    local big_geometry = { width = args.big.width, height = args.big.height }
-    for i, interface in ipairs(network_interfaces) do
-        local label = Wibox.widget.textbox()
-        label:set_markup(if_text(interface))
-        height = height + args.label_height
-        big:add(label)
-        labels[interface] = label
-        for _, typ in ipairs({"down", "up"}) do
-            local g = awful.widget.graph(big_geometry)
-            set_properties(g, {
-                border_color = nil,
-                color = args.big[typ .. '_fgcolor'],
-                background_color = args.big[typ .. '_bgcolor'] })
-            vicious.register(g, vicious.widgets.net,
-                "${" .. interface .. " " .. typ .. "_kb}", 2)
-            height = height + big_geometry.height
-            table.insert(big_widgets, g)
-            big:add(g)
-        end
-    end
+        small = Wibox.layout.fixed.horizontal()
+        small:add(small_layout[cur])
 
-    return {
-        big = {
-            widgets = big_widgets,
-            layout = big,
-            height = height,
-            width = args.big.width},
-        small = {
+        ret.small = {
             widgets = small_widgets,
             layout = small,
             height = args.small.height,
-            width = args.small.widgth } }
+            width = args.small.widgth }
+    end
+
+    local if_text = function (interface)
+        return (args.big and args.small and interface == cur) and (
+            ' <span size="x-small"><b>' .. interface .. '</b></span>') or (
+            ' <span color="#666666" size="x-small">' .. interface .. '</span>')
+    end
+
+    if args.big then
+        local labels = {}
+        if args.small then
+            small:connect_signal("button::release", function ()
+                cur = network_interfaces[interface_cache[cur] %
+                     #network_interfaces + 1]
+                small:reset()
+                small:add(small_layout[cur])
+                for _, interface in ipairs(network_interfaces) do
+                    labels[interface]:set_markup(if_text(interface))
+                end
+            end)
+        end
+
+        local height = 0
+        local big_widgets = {}
+        local big = Wibox.layout.fixed.vertical()
+        local big_geometry = { width = args.big.width, height = args.big.height }
+        for i, interface in ipairs(network_interfaces) do
+            local label = Wibox.widget.textbox()
+            label:set_markup(if_text(interface))
+            height = height + args.label_height
+            big:add(label)
+            labels[interface] = label
+            for _, typ in ipairs({"down", "up"}) do
+                local g = awful.widget.graph(big_geometry)
+                set_properties(g, {
+                    border_color = nil,
+                    color = args.big[typ .. '_fgcolor'],
+                    background_color = args.big[typ .. '_bgcolor'] })
+                vicious.register(g, vicious.widgets.net,
+                    "${" .. interface .. " " .. typ .. "_kb}", 2)
+                height = height + big_geometry.height
+                table.insert(big_widgets, g)
+                big:add(g)
+            end
+        end
+        ret.big = {
+            widgets = big_widgets,
+            layout = big,
+            height = height,
+            width = args.big.width}
+    end
+    return ret
 end
 
