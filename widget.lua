4fd24e34c3fafe670ffa508e8e1320a2016acd2c
dodo <dodo@blacksec.org>
2011-02-10 23:52:03 +0100

    option to specify the draw order of progressimage

--- widget.lua
+++ widget.lua
@@ -55,9 +55,11 @@ end
 -- @param args.y <i>(optional) </i> progressbar y offset
 -- @param args.width progressbar width
 -- @param args.width progressbar height
+-- @param args.draw_image_first <i>(default: true)</i> specify wether image or progress will be drawen first
 -- @param args.image <i>(optional) </i> image to be displayed
--- @return `wibox.widget.imagebox` with property progress with is a `awful.widget.progressbar`
+-- @return `wibox.widget.imagebox` with property progress with is a `awful.widget.progressbar`, draw_image_first, draw_progress_first, swap_first
 function progressimage(args)
+    local img_first = args.draw_image_first == nil or args.draw_image_first
     local ret = Wibox.widget.imagebox()
     ret.progress = awful.widget.progressbar(args)
 
@@ -70,14 +72,19 @@ function progressimage(args)
     local draw_image = ret.draw
     local draw_progress = ret.progress.draw
     ret.draw = function (box, wibox, cr, width, height)
-        draw_image(box, wibox, cr, width, height)
-        width  = args.width  or width
-        height = args.height or height
+        if img_first then draw_image(box, wibox, cr, width, height) end
+        local w = args.width  or width
+        local h = args.height or height
         cr:save()
         cr:translate(ret.progress.x, ret.progress.y)
-        draw_progress(ret.progress, wibox, cr, width, height)
+        draw_progress(ret.progress, wibox, cr, w, h)
         cr:restore()
+        if not img_first then draw_image(box, wibox, cr, width, height) end
     end
+
+    ret.draw_image_first    = function ()  img_first = true   end
+    ret.draw_progress_first = function ()  img_first = false  end
+    ret.swap_first  = function ()  img_first = not img_first  end
 
     return ret
 end
