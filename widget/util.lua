133b62fd17beb7590d2895ada51ceb8877d8736d
dodo <dodo@blacksec.org>
2011-03-05 18:26:40 +0100

    add some control functions to uzful.widget.infobox

--- widget/util.lua
+++ widget/util.lua
@@ -6,8 +6,10 @@
 
 local util = require("uzful.util")
 local Wibox = require("wibox")
+local setmetatable = setmetatable
 local pairs = pairs
-
+local type = type
+local capi = { screen = screen }
 
 module("uzful.widget.util")
 
@@ -52,6 +54,71 @@ end
 -- @param args any wibox args like in uzful.widget.util.wibox
 -- @return wibox object
 function infobox(args)
-    return wibox(util.table.update({ type = "notification",
+    local box = wibox(util.table.update({ type = "notification",
         visible = false, ontop = true }, args))
+    local ret = {}
+    local size = args.size
+    local align = args.align or "left"
+    local position = args.position or "top"
+
+    ret.update = function ()
+        local screen = screen or box.screen or 1
+        local area = capi.screen[screen].workarea
+
+        if size and type(size) == "function" then
+            box.width, box.height = size()
+        end
+
+        if position == "top" then
+            box.y = area.y
+        elseif position == "center" then
+            box.y = area.y + (area.height - box.height) * 0.5
+        elseif position == "bottom" then
+            box.y = area.y + area.height - box.height
+        end
+
+        if align == "left" then
+            box.x = area.x
+        elseif align == "center" then
+            box.x = area.x + (area.width - area.width) * 0.5
+        elseif align == "right" then
+            box.x = area.x + area.width - box.width
+        end
+    end
+    ret.toggle = function ()
+        box.visible = not box.visible
+    end
+    ret.hide = function ()
+        if box.visible then
+            box.visible = false
+        end
+    end
+    ret.show = function ()
+        if not box.visible then
+            box.visible = true
+        end
+    end
+    ret.set_align = function (m, mode)
+        mode = mode or m
+        local allowed = { left = true, center = true, right = true }
+        if allowed[mode] then
+            align = mode
+            ret:update()
+        end
+    end
+    ret.set_position = function (m, mode)
+        mode = mode or m
+        local allowed = { top = true, center = true, bottom = true }
+        if allowed[mode] then
+            position = mode
+            ret:update()
+        end
+    end
+
+    setmetatable(ret, {
+        __index = box,
+        __newindex = box
+    })
+    ret:update()
+    return ret
 end
