14a40f632bd29e862026275fa3688a9f63f25ff5
dodo <dodo.the.last@gmail.com>
2013-07-17 12:14:11 +0200

    first wörking notification hider

--- notifications/init.lua
+++ notifications/init.lua
@@ -4,24 +4,27 @@
 -- @release v3.4-503-g4972a28
 --------------------------------------------------------------------------------
 
+local os = require('os')
 local wibox = require("wibox")
-local util = require("awful.util")
-local menu = require("uzful.menu.popup")
+local awful = require("awful")
 local naughty = require("naughty")
 local vicious = require("vicious")
 local helpers = require("vicious.helpers")
 local beautiful = require("beautiful")
+local uzful = { widget = { scroll = require('uzful.widget.scroll') } }
 local setmetatable = setmetatable
 local ipairs = ipairs
 local pairs = pairs
 local table = table
 local widgets = {}
-
+local capi = {
+    mouse = mouse,
+    screen = screen }
 
 module("uzful.notifications")
 
-
-data = {}
+local mt = {}
+local data = {}
 
 function patch()
     local notify = naughty.notify
@@ -66,7 +69,7 @@ function update(notification, args)
 end
 
 
-function add(wid, args)
+function mt.add(wid, args)
     local conf = widgets[wid]
     if conf == nil or not conf.visible then return end
     wid.number = wid.number + 1
@@ -78,56 +81,91 @@ function add(wid, args)
     local item
     local mouse_fun = function (menuitem, menu)
         wid.number = wid.number - 1
+        args.notification.box.visible = true
         args.notification.die()
-        menu:delete(menuitem)
         setMarkup()
-        local i = util.table.hasitem(args)
+        local i = awful.util.table.hasitem(args)
         if i then
             table.remove(data, i)
         end
-        if #menu.items * (menu.height + menu.theme.border_width) +
-         menu.theme.border_width < menu.max_height then
-            menu.scroll.offset = 0
-            menu:update()
+        return (wid.number ~= 0), function ()
+            menu:delete(menuitem)
+            if #menu.items * (menu.height + menu.theme.border_width) +
+                menu.theme.border_width < menu.layout.max then
+                menu.layout:set_offset(0)
+                if wid.number then
+                    menu:update()
+                else
+                    self:hide()
+                end
+            end
         end
-        return wid.number ~= 0
     end
-    wid.menu:add({
-        theme = args.theme or {}, args.text or "", mouse_fun, args.icon }, 1)
+    local text = os.date("[%H:%M:%S]  ") .. (args.text or "")
+    wid.menu:add({ theme = args.theme or {}, text, mouse_fun, args.icon }, 1)
     wid.menu:update()
 end
 
 
-function show(wid, args)
+function mt.show(wid, args)
     local conf = widgets[wid]
     if conf == nil then return end
     if conf.visible then
         conf.menu_args = args
-        wid.menu.scroll.offset = 0
+        wid.menu.layout:set_offset(0)
+        wid.menu.layout.timer:start()
+        vid.menu:update()
         wid.menu:show(args)
     end
 end
 
 
-function hide(wid)
+function mt.hide(wid)
     local conf = widgets[wid]
     if conf == nil then return end
     if conf.visible then
         wid.menu:hide()
+        wid.menu.layout.timer:stop()
     end
 end
 
 
-function toggle_menu(wid, args)
+function mt.toggle_menu(wid, args)
     local conf = widgets[wid]
     if conf == nil then return end
     if conf.visible then
+--         local x, y = wid.menu.wibox.x, wid.menu.wibox.y
         wid.menu:toggle(args)
+        local w, h = wid.menu.wibox.width, wid.menu.wibox.height
+        wid.menu.layout.size = { width = w , height = h }
+        w, h = wid.menu.layout:fit(w, h)
+        wid.menu.width, wid.menu.height = w, h
+        wid.menu.wibox.width, wid.menu.wibox.height = w, h
+        
+        if wid.menu.wibox.visible then
+            
+            local geo = capi.screen[capi.mouse.screen].workarea
+            local coords = capi.mouse.coords()
+            coords.x = coords.x + 1
+            coords.y = coords.y + 1
+            wid.menu.wibox.x = coords.x < geo.x and geo.x or coords.x
+            wid.menu.wibox.y = coords.y < geo.y and geo.y or coords.y
+            
+            local a, b, la, lb = "x", "y", "width", "height"
+            if wid.menu.layout.dir == "vertical" then a, b, la, lb = b, a, lb, la end
+            local screen_s = geo[b] + geo[lb]
+            wid.menu.wibox[b] = wid.menu.wibox[b] + wid.menu.wibox[lb] > screen_s and
+                    screen_s - wid.menu.wibox[lb] or wid.menu.wibox[b]
+    
+            wid.menu.layout.timer:start()
+        else
+            wid.menu.layout.timer:stop()
+        end
     end
 end
 
 
-function enable(wid)
+function mt.enable(wid)
     local conf = widgets[wid]
     if conf == nil or conf.visible then return end
     conf.visible = true
@@ -136,7 +174,7 @@ function enable(wid)
 end
 
 
-function disable(wid)
+function mt.disable(wid)
     local conf = widgets[wid]
     if conf == nil or not conf.visible then return end
     local new = {}
@@ -152,13 +190,13 @@ function disable(wid)
     data = new
     wid.number = 0
     conf.visible = false
-    wid.menu.scroll.offset = 0
+    wid.menu.layout:set_offset(0)
     wid.text:set_markup(conf.disabled)
     naughty.resume()
 end
 
 
-function toggle(wid)
+function mt.toggle(wid)
     local conf = widgets[wid]
     if conf == nil then return end
     if conf.visible then
@@ -172,6 +210,7 @@ end
 function new(screen, args)
     screen = screen or 1
     args = args or {}
+    local ret
 
     local conf = {
         disabled = helpers.format(args.disabled or "$1", { "⤫" }),
@@ -180,20 +219,34 @@ function new(screen, args)
         visible = args.visible ~= nil and args.visible,
         screen = screen }
     conf.menu.max = conf.menu.max or args.max or 345
+    conf.menu.layout = conf.menu.layout or function (...)
+        local layout = uzful.widget.scroll("vertical", conf.menu.max, {
+            widget = wibox.layout.flex.vertical(...),
+            fit = nil,
+        })
+        layout.add   = function (scroll, ...) return scroll.widget:add(...) end
+        layout.reset = function (scroll, ...) return scroll.widget:reset(...) end
+        return layout
+    end
 
-    local ret = {
-        menu = menu(conf.menu),
+    ret = {
+        menu = awful.menu(conf.menu),
         text = wibox.widget.textbox(),
-        toggle_menu = toggle_menu,
-        disable = disable,
-        enable = enable,
-        toggle = toggle,
-        number = 0,
-        show = show,
-        hide = hide,
-        add = add }
+        number = 0 }
+    setmetatable(ret, mt)
     widgets[ret] = conf
     conf.menu.len = #ret.menu.items
+    local update = ret.menu.update
+    ret.menu.update = function (...)
+        local x, y = ret.menu.wibox.x, ret.menu.wibox.y
+        update(...)
+        local w, h = ret.menu.wibox.width, ret.menu.wibox.height
+        ret.menu.layout.size = { width = w , height = h }
+        w, h = ret.menu.layout:fit(w, h)
+        ret.menu.wibox.x, ret.menu.wibox.y = x, y
+        ret.menu.width, ret.menu.height = w, h
+        ret.menu.wibox.width, ret.menu.wibox.height = w, h
+    end
 
     for _, v in pairs(data) do
         if v.screen == screen then
@@ -207,5 +260,5 @@ function new(screen, args)
     return ret
 end
 
-
+mt = { __index = mt }
 setmetatable(_M, { __call = function (_, ...) return new(...) end })
---
+++ widget/scroll.lua
@@ -0,0 +1,171 @@
+---------------------------------------------------------------------------
+-- @author Uli Schlachter
+-- @author dodo
+-- @copyright 2013 dodo
+-- @release @AWESOME_VERSION@
+---------------------------------------------------------------------------
+
+local widget = require("wibox.widget")
+local color = require("gears.color")
+local layout = require("wibox.layout")
+local surface = require("gears.surface")
+local cairo = require("lgi").cairo
+local setmetatable = setmetatable
+local pairs = pairs
+local type = type
+local capi = { timer = timer }
+
+-- uzful.widget.scroll
+local scroll = { mt = {} }
+
+local function arrow(ret, text, offset)
+    local label = widget.textbox()
+    label:set_markup(text)
+    label:set_align("center")
+    label:set_valign("center")
+    local arrow = layout.rotate(label, ret.dir == "vertical" and "north" or "east")
+    arrow:connect_signal("mouse::enter", function ()
+        ret:scroll_with(offset, false)
+    end)
+    arrow:connect_signal("mouse::leave", function ()
+        ret:scroll_with(0, true)
+    end)
+    return arrow
+end
+
+
+
+--- Draw this widget
+function scroll:draw(wibox, cr, width, height)
+    if not self.widget then
+        return
+    end
+    layout.base.draw_widget(wibox, cr, self.control, 0, 0, width, height)
+end
+
+
+function scroll:set_offset(offset)
+    self.offset[self.dir == "vertical" and "y" or "x"] = offset
+end
+
+
+--- Fit this widget into the given area
+function scroll:fit(width, height)
+    if not self.widget then
+        return 0, 0
+    end
+    if self.inner_fit then width, height = self:inner_fit(width, height) end
+    local s, a, b = { w = width, h = height }, "w", "h"
+    if self.dir == "vertical" then a, b = b, a end
+    if s[a] < 1 then s[a] = 1 end
+        if self.control.third then s[a] = s[a] + 9 end -- FIXME 9 is the height of the arrow
+    s.w, s.h = self.widget:fit(s.w, s.h)
+    if self.control.third then s[a] = s[a] + 9 end -- FIXME 9 is the height of the arrow
+    if s[a] > self.max then s[a] = self.max end
+    self.cur = s
+    return s.w, s.h
+end
+
+--- Set the widget that is drawn on top of the background
+function scroll:set_widget(...)
+    self.control:set_second(...)
+    self.widget = self.control.second
+    if self.widget.__hooked == self then return end
+    self.widget.__hooked = self
+    local draw_widget = self.widget.draw
+    self.widget.draw = function (widget, wibox, cr, width, height)
+        local a, b, s = "w", "h", { w = self.size.width, h = self.size.height }
+        if self.dir == "vertical" then a, b = b, a end
+        cr:save()
+        cr:translate(-self.offset.x, -self.offset.y)
+        draw_widget(widget, wibox, cr, s.w, s.h)
+        cr:restore()
+    end
+end
+
+
+function scroll:scrolling()
+    if self.by == 0 then return end
+    local offset = self.offset[self.dir == "vertical" and "y" or "x"] + self.by
+    if offset <= 0 then
+        offset = 0
+    else
+        local size = self.cur[self.dir == "vertical" and "h" or "w"]
+        local _max = self.size[self.dir == "vertical" and "height" or "width"]
+        size = size - 9 - 9 -- FIXME 9 is the height of the arrow
+        if offset + size > _max then
+            offset = _max - size
+        end
+    end
+    self.offset[self.dir == "vertical" and "y" or "x"] = offset
+    if self._singleshot then
+        self._singleshot = false
+        self.by = 0
+    end
+    self:emit_signal("widget::updated")
+end
+
+
+function scroll:scroll_with(offset, singleshot)
+    self.by = offset or 0
+    self._singleshot = singleshot
+end
+
+
+function scroll:scroll_by(offset, singleshot)
+    self.by = self.by + (offset or 0)
+    self._singleshot = singleshot
+end
+
+
+--- Returns a new scroll area layout.
+-- @param dir scroll direction
+-- @param max_size max size in direction (optional)
+-- @param args.widget The widget to display (optional)
+local function new(dir, max_size, args)
+    args = args or {}
+    local ret = widget.base.make_widget()
+    local timer = capi.timer({ timeout = args.scroll_every or 0.05 })
+    timer:connect_signal("timeout", function ()
+        ret:scrolling()
+    end)
+    ret.by = 0
+    ret.timer = timer
+    ret._singleshot = false
+    ret.dir = dir
+    ret.max = max_size
+    ret.inner_fit = args.fit
+    ret.offset = { x = 0, y = 0 }
+    ret.size   = { width = 0, height = 0 }
+
+    for k, v in pairs(scroll) do
+        if type(v) == "function" then
+            ret[k] = v
+        end
+    end
+
+    ret._emit_updated = function()
+        ret:emit_signal("widget::updated")
+    end
+
+    ret.control = layout.align[dir]()
+    ret.arrow = {
+        up =   arrow(ret, args.up_arrow   or "▴", -(args.scroll_by or 8)),
+        down = arrow(ret, args.down_arrow or "▾",  (args.scroll_by or 8)),
+    }
+    ret.control:set_first(ret.arrow.up)
+    ret.control:set_third(ret.arrow.down)
+    ret.control:connect_signal("widget::updated", ret._emit_updated)
+    
+    ret:set_widget(args.widget)
+
+    return ret
+end
+
+function scroll.mt:__call(...)
+    return new(...)
+end
+
+return setmetatable(scroll, scroll.mt)
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
