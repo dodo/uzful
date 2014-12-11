f7388a01579f64975fe1088c875cc78613507862
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-12-11 02:16:18 +0100

    add string helpers to util

--- util.lua
+++ util.lua
@@ -4,7 +4,7 @@
 -- @release v3.4-503-g4972a28
 --------------------------------------------------------------------------------
 
-util = { module = {} }
+local util = { module = {}, string = {} }
 
 local io = require("io")
 local obvious = {}
@@ -230,6 +230,34 @@ function util.module.exists(pkg, name)
         end
     end
     return false
+end
+
+function util.string.gsplit(s, sep, plain)
+    local start = 1
+    local done = false
+    local function pass(i, j, ...)
+        if i then
+            local seg = s:sub(start, i - 1)
+            start = j + 1
+            return seg, ...
+        else
+            done = true
+            return s:sub(start)
+        end
+    end
+    return function()
+        if done then return end
+        if sep == '' then done = true return s end
+        return pass(s:find(sep, start, plain))
+    end
+end
+
+function util.lineswrap(s, n)
+    local lines = {}
+    for line in util.string.gsplit(s, "\n") do lines[#lines+1] = line end
+    while #lines > n do table.remove(lines, 1) end
+    if #lines == 0 then return "\n" end
+    return table.concat(lines, "\n")
 end
 
 
